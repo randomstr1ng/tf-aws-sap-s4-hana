1e93f3438b2b1cdef9d126bd7bbae984fa33ca15
randomstr1ng <julian@petersohn.it>
2024-01-17 09:53:24 +0100

    rename output of instance id

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "private_ip" {
   value = aws_instance.ec2_instance.private_ip
 }
 
-output "sapcloudconnector_instance_id" {
+output "s4-hana_instance_id" {
   value = aws_instance.ec2_instance.id 
 }
